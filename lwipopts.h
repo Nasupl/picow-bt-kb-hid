1b96aac91907188723781939b3761485617c00b3
Kazuya N <7274407+Nasupl@users.noreply.github.com>
2026-08-15 06:51:09 +0900

    Match Pico SDK lwIP network options

--- lwipopts.h
+++ lwipopts.h
@@ -13,10 +13,13 @@
 #define LWIP_ETHERNET 1
 #define LWIP_ICMP 1
 #define LWIP_RAW 1
+#define LWIP_CHKSUM_ALGORITHM 3
+#define LWIP_DHCP 1
 #define LWIP_IPV4 1
 #define LWIP_TCP 1
 #define LWIP_UDP 1
 #define LWIP_DNS 1
+#define LWIP_TCP_KEEPALIVE 1
 #define TCP_MSS 1460
 #define TCP_WND (8 * TCP_MSS)
 #define TCP_SND_BUF (8 * TCP_MSS)
@@ -30,5 +33,7 @@
 #define MEMP_STATS 0
 #define LINK_STATS 0
 #define LWIP_NETIF_TX_SINGLE_PBUF 1
+#define DHCP_DOES_ARP_CHECK 0
+#define LWIP_DHCP_DOES_ACD_CHECK 0
 
 #endif
